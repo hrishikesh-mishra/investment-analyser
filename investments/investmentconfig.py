042441b8b924bebde683bc6ddfe2fbd6829d6759
hrishikesh-mishra <sd.hrishi@gmail.com>
2024-12-30 12:23:49 +0530

    sample config

---
+++ investments/investmentconfig.py
@@ -0,0 +1,9 @@
+## Coode ->  
+## ticker 
+
+INVESTMENTS = [
+	{'code':'120586','name': 'ICICI Prudential Bluechip Fund - Direct Plan - Growth', 'ticker' : "0P0000XWAT.BO" },
+	{'code':'125497', 'name': 'SBI Small Cap Fund - Direct Plan - Growth', 'ticker' : '0P0000XW1B.BO'},
+	{'code':'125494', 'name': 'SBI Small Cap Fund - Regular Plan - Growth', 'ticker': '0P0000KV39.BO'}
+]
+
