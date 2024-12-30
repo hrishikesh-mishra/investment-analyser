e6b95b42dbd904b4335b9a0860ed94e7af3ca656
hrishikesh-mishra <sd.hrishi@gmail.com>
2024-12-30 12:10:59 +0530

    clean up

--- investments/investmentconfig.py
+++
@@ -1,9 +0,0 @@
-## Code -> https://raw.githubusercontent.com/NayakwadiS/mftool/master/data/Scheme_codes.txt
-## Ticker -> Yahoo finance ticker. you can find here https://github.com/NayakwadiS/Forecasting_Mutual_Funds/blob/master/codes.json
-
-INVESTMENTS = [
-	{'code': '120586','name': 'ICICI Prudential Bluechip Fund - Direct Plan - Growth', 'ticker' : "0P0000XWAT.BO" },
-	{'code': '125497', 'name': 'SBI Small Cap Fund - Direct Plan - Growth', 'ticker' : '0P0000XW1B.BO'},
-	{'code': '125494', 'name': 'SBI Small Cap Fund - Regular Plan - Growth', 'ticker': '0P0000KV39.BO'},
-]
-
